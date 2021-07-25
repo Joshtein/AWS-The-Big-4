83912cf34d1167e49ab250f971307bd80f24f104
HardyValen <hardyvalen@gmail.com>
2021-07-25 22:54:43 +0700

    25 jul 2021 - newsletter

--- src/components/landing/Landing.js
+++ src/components/landing/Landing.js
@@ -2,12 +2,29 @@ import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import LandingNavbar from '../LandingNavbar';
 import LandingHero from './_LandingHero';
+import { Box, Grid } from '@material-ui/core';
+import News from './_News';
+import FeaturedRecommendation from './_FeaturedRecommendation';
 
 function Landing() {
   return (
     <div>
       <LandingNavbar/>
       <LandingHero/>
+      <Box p={6}>
+        <Grid container>
+          <Grid item md={7}>
+            <Box p={2}>
+              <News/>
+            </Box>
+          </Grid>
+          <Grid item md={5}>
+            <Box p={2}>
+              <FeaturedRecommendation/>
+            </Box>
+          </Grid>
+        </Grid>
+      </Box>
     </div>
   )
 }
---
+++ src/components/landing/_FeaturedRecommendation.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Fragment } from 'react';
+import Title from './Title';
+
+function FeaturedRecommendation() {
+  return (
+    <Fragment>
+      <Title>Featured Recommendation</Title>
+    </Fragment>
+  )
+}
+
+export default FeaturedRecommendation;
